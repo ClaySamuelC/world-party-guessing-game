8484384dbbf21ee162031a18ef716cd48fde01af
wpgg-deploy <deploy@localhost>
2026-09-19 09:56:31 -0500

    Update site from 2bebd69

---
+++ assets/pl-C_csnbBF.js
@@ -0,0 +1,7 @@
+var e=`<svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-pl" viewBox="0 0 640 480">
+  <g fill-rule="evenodd">
+    <path fill="#fff" d="M640 480H0V0h640z"/>
+    <path fill="#dc143c" d="M640 480H0V240h640z"/>
+  </g>
+</svg>
+`;export{e as default};
